5e67cb1880c06eaff24c1d136ff163fe46c2998c
ibrodowski <ian.brodowski@reflektion.com>
2016-06-28 17:05:15 -0700

    Revision 6
    Updated revision history

--- AUTOMATION/automate.sh
+++ AUTOMATION/automate.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #
 # Author	  : Ian Brodowski
-# Last Update     : Sunday, June 19, 2016
+# Last Update     : Tuesday, June 28, 2016
 #
 # Requirements    : Requires software packages to be located within the same directory as the script.
 #                 : This script must be executed under sudo.
 #
 # Change History  :
+#   * 20160628    : Added delta updates for MS Excel, PowerPoint and Word
+#
 #   * 20160619    : Removed Bitdefender for Standard image
 #
 #   * 20160618    : Added MS Excel and MS Word Delta Updates
