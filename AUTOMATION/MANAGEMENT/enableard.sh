241e7eed5e3b064b85b33fc17857e6f0960c5d6c
ibrodowski <ian.brodowski@reflektion.com>
2016-07-06 18:04:46 -0700

    Revision 1
    Activate and enable ARD for local admin account

---
+++ AUTOMATION/MANAGEMENT/enableard.sh
@@ -0,0 +1,5 @@
+#
+#!/bin/bash
+
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -allowAccessFor -specifiedUsers
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -access -on -privs -all users rfkadmin
