ba3a0a592180b317baf098acfac735db43b3a8af
ibrodowski <ian.brodowski@reflektion.com>
2016-07-06 18:05:41 -0700

    Revision 2
    Corrected typo on line 5, users should have been -users

--- AUTOMATION/MANAGEMENT/enableard.sh
+++ AUTOMATION/MANAGEMENT/enableard.sh
@@ -2,4 +2,4 @@
 #!/bin/bash
 
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -allowAccessFor -specifiedUsers
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -access -on -privs -all users rfkadmin
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -access -on -privs -all -users admin_account_name
