94a301403322a70d9613db91a781cdf92fb3934e
ibrodowski <ian.brodowski@reflektion.com>
2016-10-06 18:03:51 -0700

    Delete enableard.sh

--- AUTOMATION/MANAGEMENT/enableard.sh
+++
@@ -1,7 +0,0 @@
-#
-#!/bin/bash
-
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -clientopts -setvnclegacy -vnclegacy -no -setreqperm -reqperm no -setmenuextra -menuextra no
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -users 'admin_account_name' -access -on -privs -all 
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -specifiedUsers
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -menu
